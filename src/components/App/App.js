393bbc8557a567f320b7df346663a543bf8b5f7c
Your Name <you@example.com>
2022-06-16 15:49:46 +0200

    Add options.js, added new style, and create new localStorage in Options.js or App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,24 +5,35 @@ import AllPost from '../../components/AllPost/AllPostContainer';
 import AddPost from '../AddPost/AddPostContainer';
 import Post from '../Post/PostContainer';
 import Options from '../Options/Options';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-  
-      </div>
 
-      <Routes>
-        <Route path='/' element={<Layout />}>
+  let[fontSize, setFontSize] = useState('16')
+  let lsFontSize = localStorage.getItem('fontSize')
+  let lsColor = localStorage.getItem('color')
+
+  useEffect(() => {
+    setFontSize(lsFontSize)
+    document.body.style= `background-color: ${lsColor}`
+  }, [])
+
+  return (
+    <div className="App" style={{fontSize: `${fontSize}px`}} >
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
             <Route index element={<AllPost />}/>
             <Route path="/post/:id" element={<Post />} />
             <Route path="/add-post" element={<AddPost/>} />
             <Route path="settings" element={<Options />} />
-        </Route>
-      </Routes>
-
+          </Route>
+        </Routes>
     </BrowserRouter>
+
+    </div>
   );
 }
 
--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import styles from '../Options/Options.module.scss';
 
 const Options = () => {
+
+    const[fontSize, setSize] = useState(Number(localStorage.getItem('fontSize')) || 16)
+    const[colorBackGround, setColor] = useState(localStorage.getItem('color') || '#fff')
+
+    const handleChanges = () => {
+        localStorage.setItem('color', colorBackGround);
+        localStorage.setItem('fontSize', fontSize);
+        window.location.reload(false)
+    }
+
     return(
         <div className={styles.optionWrapper}>
             <h2 className={styles.title}>Ustawienia</h2>
@@ -10,8 +21,10 @@ const Options = () => {
                 <div className={styles.option}>
                     <p className={styles.optionTitle}>Wielkosc tekstu</p>
                     <input
-                        type="number"
+                        value={fontSize} 
+                        type="number" 
                         className={styles.sizeInput}
+                        onChange={e=>setSize(Number(e.target.value))}
                     >
                     </input>
                 </div>
@@ -19,8 +32,10 @@ const Options = () => {
                 <div className={styles.option}>
                     <p className={styles.optionTitle}>Kolor tła strony</p>
                     <input
-                        type="color"
+                        value={colorBackGround} 
+                        type="color" 
                         className={styles.colorInput}
+                        onChange={e=>setColor(e.target.value)}
                     >
                     </input>
                 </div>
@@ -30,6 +45,7 @@ const Options = () => {
 
                     type="button"
                     className={styles.btnSave}
+                    onClick={handleChanges}
                     >
                     <span className={styles.SaveStyl}>Zapisz</span>
                     </button>
